ed9bc758bb2db65c172fe53d8588dabe04c89b04
Iain Goh <iaingoh@gmail.com>
2018-12-28 18:25:38 +0800

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Credit Card Payment Form in HTML & CSS
---
+++ js/app.js
@@ -0,0 +1,3 @@
+// $(function() {});
+// var monthControl = document.querySelector('input[type="month"]');
+// monthControl.value = '1978-06';
